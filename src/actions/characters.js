d1f3b1b6881f4a2bf2712da65637a44bf55c14ee
Nick Lontorfos <nick.lontorfos@adidas.com>
2021-11-03 16:54:33 +0100

    basic redux working

--- src/actions/characters.js
+++ src/actions/characters.js
@@ -1,11 +1,23 @@
 import { getCharacters } from '../api/characters'
+import { GET_CHARACTERS, GET_CHARACTERS_FAILURE, GET_CHARACTERS_SUCCESS } from '../constants'
 
+// this in reality is an action creator
 export const getCharactersAction = () => {
   return async dispatch => {
-    const characters = await getCharacters()
     dispatch({
-      type: 'GET_CHARACTERS',
-      data: characters.data.results
+      type: GET_CHARACTERS
     })
+    try {
+      const characters = await getCharacters()
+      dispatch({
+        type: GET_CHARACTERS_SUCCESS,
+        data: characters.data.results
+      })
+    } catch (e) {
+      dispatch({
+        type: GET_CHARACTERS_FAILURE,
+        data: e
+      })
+    }
   }
 }
--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,7 +1,13 @@
+import { GET_CHARACTERS, GET_CHARACTERS_FAILURE, GET_CHARACTERS_SUCCESS } from '../constants'
+
 const charactersReducer = (state = { data: null, loading: false, error: null }, action) => {
   switch (action.type) {
-    case 'GET_CHARACTERS':
+    case GET_CHARACTERS:
+      return { data: null, loading: true, error: null }
+    case GET_CHARACTERS_SUCCESS:
       return { data: action.data, loading: false, error: null }
+    case GET_CHARACTERS_FAILURE:
+      return { data: null, loading: false, error: action.error }
     default:
       return state
   }
